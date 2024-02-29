4cb9b335fd9d70ca09c9a14c8ebb84a2cd5a7b2a
laimax17 <caicai0660@gmail.com>
2024-02-29 06:00:28 +0000

    activity

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,39 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function findNegative(arr, rowIndex) {
+    return new Promise((resolve, reject) => {
+        if (arr.length < rowIndex) {
+            setTimeout(() => {
+                let neg = False;
+                for (let i = 0; i < arr[rowIndex].length; i ++) {
+                    if (arr[rowIndex][i] < 0) {
+                        neg = True;
+                    }
+                } if (neg) {
+                    resolve(rowIndex);
+                } else {
+                    resolve("no negatives");
+                }
+            }, 0);
+        }
+        else {
+            reject(`Row Index ${rowIndex} must be within 0 and ${arr.length}`)
+        }
+    })
+}
+
+negPromises = [];
+for (let x = 0; x < array2D.length; x++) {
+    negPromises.push(findNegative(array2D, x))
+}
+
+Promise.all(negPromises)
+    .then((negs) => {
+        let rows = []
+        negs.forEach(item => {
+            rows.push(item) })
+        console.log(`Sum = ${sum}`)
+    })
+    .catch((error) => console.log(`Error: ${error}`));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,35 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumOfRow(arr, rowIndex) {
+    return new Promise((resolve, reject) => {
+        if (arr.length < rowIndex) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr[rowIndex].length; i ++) {
+                    sum += arr[rowIndex][i];
+                }
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            reject(`Row Index ${rowIndex} must be within 0 and ${arr.length}`)
+        }
+    })
+}
+
+rowsumPromises = [];
+for (let x = 0; x < array2D.length; x++) {
+    rowsumPromises.push(sumOfRow(array2D, x))
+}
+
+Promise.all(rowsumPromises)
+    .then((rowSums) => {
+        let sum = 0;
+        rowSums.forEach(rowSum => {
+            sum += rowSum;
+        })
+        console.log(`Sum = ${sum}`)
+    })
+    .catch((error) => console.log(`Error: ${error}`));
